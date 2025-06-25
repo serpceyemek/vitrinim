dae8bf35bb0fa42d1911eecf891f0bbb145430e5
serpceyemek <serpceyemek@gmail.com>
2025-06-25 03:27:22 +0300

    “Add src/App.jsx”

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Vitrinim</h1>
+      <p>Demo amaçlı başlangıç sayfası.</p>
+    </div>
+  );
+}
+
+export default App;
