4f0911e8fa27de96f6cb0cd7bdf30dc186e86ccc
serpceyemek <serpceyemek@gmail.com>
2025-06-26 16:20:45 +0300

    Footer bileşeni eklendi

---
+++ src/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer style={{ padding: '1rem', background: '#f1f1f1', textAlign: 'center' }}>
+    <p>© 2025 Vitrinim. Tüm hakları saklıdır.</p>
+  </footer>
+);
+
+export default Footer;
