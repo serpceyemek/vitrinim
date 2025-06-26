546bba24bc6e35c5f94899a8e190f95402dc5f62
serpceyemek <serpceyemek@gmail.com>
2025-06-26 20:57:28 +0300

    components klasörü yeniden oluşturuldu

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Vitrinim</h1>
+      <nav>
+        <a href="/">Ana Sayfa</a> | <a href="/ilanlar">İlanlar</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
