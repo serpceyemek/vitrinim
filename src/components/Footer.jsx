1fad87b20bd68201f6b5721a0a3d33d91e43e2d6
serpceyemek <serpceyemek@gmail.com>
2025-06-28 16:42:36 +0300

    Footer görsel güncellendi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
 
-const Footer = () => {
-  return (
-    <footer>
-      <p>© {new Date().getFullYear()} Vitrinim - Tüm hakları saklıdır.</p>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer style={{ marginTop: '2rem', padding: '1rem', backgroundColor: '#f1f1f1', textAlign: 'center' }}>
+    <p>© 2025 Vitrinim. Tüm hakları saklıdır.</p>
+  </footer>
+);
 
 export default Footer;
