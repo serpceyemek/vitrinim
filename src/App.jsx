b975f0db6e74b9e05aa731ad85a5ca7a2afd1e3d
Serpil Bayrak <serpce_11@hotmail.com>
2025-09-01 21:06:53 +0300

    simplify App: render Home directly

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-
-function Home() {
-  return <h1>Home Page</h1>;
-}
-
-function About() {
-  return <h1>About Page</h1>;
-}
+import Home from "./pages/Home";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  );
+  return <Home />;
 }
