ff1acfd56b10c80041bf52e041f478c72b50a4c3
Serpil Bayrak <serpce_11@hotmail.com>
2025-09-02 16:51:08 +0300

    chore(data): placeholder görsellerine metin eklendi

---
+++ src/data/listings.js
@@ -0,0 +1,17 @@
+// src/data/listings.js
+const listings = [
+  {
+    id: 1,
+    title: "Eviniz Burada!",
+    price: 120000,
+    image: "https://placehold.co/640x400?text=Emlak",
+  },
+  {
+    id: 2,
+    title: "Otomobil Fırsatı",
+    price: 85000,
+    image: "https://placehold.co/640x400?text=Otomobil",
+  },
+];
+
+export default listings;
