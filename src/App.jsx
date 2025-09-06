7a02e86e3a65d0d5dbbbafaefc45d47c577a33fb
Serpil Bayrak <serpce_11@hotmail.com>
2025-09-06 04:10:59 +0300

    fix: categories import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
-import Categories from "./pages/Categories.jsx";
+import Categories from "./pages/categories.jsx";
 import Category from "./pages/Category.jsx";
 import ListingDetail from "./pages/ListingDetail.jsx";
 import NewListing from "./pages/NewListing.jsx";
