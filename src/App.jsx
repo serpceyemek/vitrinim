f00f249dc7110059ea90eeb9c8f9f15c25e161d2
Serpil Bayrak <serpce_11@hotmail.com>
2025-09-12 02:39:48 +0300

    fix: default export App + index mounts BrowserRouter; remove stray App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-// src/index.js
 import React from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { Routes, Route } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Categories from "./pages/categories";
+import Category from "./pages/Category";
+import ListingDetail from "./pages/ListingDetail";
+import NewListing from "./pages/NewListing";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main style={{ maxWidth: 1200, margin: "0 auto", padding: "16px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categories" element={<Categories />} />
+          <Route path="/category/:path" element={<Category />} />
+          <Route path="/ilan/:id" element={<ListingDetail />} />
+          <Route path="/new" element={<NewListing />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+}
