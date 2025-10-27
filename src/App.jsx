9155228f00b8a7dec60b277d4874ec2da73d0b06
serpceyemek <serpce_11@hotmail.com>
2025-10-28 01:25:37 +0300

    fix(router): çift BrowserRouter kaldırıldı, App.jsx sadeleştirildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+// src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import Home from "./pages/Home";
@@ -10,18 +11,16 @@ import BottomTabs from "./components/BottomTabs";
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/magaza" element={<Magaza />} />
-          <Route path="/ilan-ver" element={<NewListing />} />
-          <Route path="/onizleme" element={<Onizleme />} />
-        </Routes>
+    <div className="min-h-screen bg-gray-50">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/magaza" element={<Magaza />} />
+        <Route path="/ilan-ver" element={<NewListing />} />
+        <Route path="/onizleme" element={<Onizleme />} />
+      </Routes>
 
-        <Toaster position="bottom-center" />
-        <BottomTabs />
-      </div>
-    </Router>
+      <Toaster position="bottom-center" />
+      <BottomTabs />
+    </div>
   );
 }
