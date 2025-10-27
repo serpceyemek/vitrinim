3519e91e0ad0d935c5b3cb7d3c4637cf620a82c4
serpceyemek <serpce_11@hotmail.com>
2025-10-28 01:20:32 +0300

    fix(ui): App.jsx router yapısını temizledim, gereksiz StepForm importunu kaldırdım

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+
 import Home from "./pages/Home";
 import Magaza from "./pages/Magaza";
-import StepForm from "./components/StepForm";
 import Onizleme from "./pages/Onizleme";
+import NewListing from "./pages/NewListing";
 import BottomTabs from "./components/BottomTabs";
 
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50 pb-20">
+      <div className="min-h-screen bg-gray-50">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/magaza" element={<Magaza />} />
-          <Route path="/ilan-ver" element={<StepForm />} />
+          <Route path="/ilan-ver" element={<NewListing />} />
           <Route path="/onizleme" element={<Onizleme />} />
         </Routes>
+
+        <Toaster position="bottom-center" />
         <BottomTabs />
       </div>
-      <Toaster position="bottom-center" toastOptions={{ duration: 1500 }} />
     </Router>
   );
 }
