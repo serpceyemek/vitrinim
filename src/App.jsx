0a2d0523b26e059f541c098c9d2b85435c268ade
serpceyemek <serpce_11@hotmail.com>
2025-10-28 16:12:45 +0300

    build sonrası temiz deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 // SAYFALAR
 import Home from "./pages/Home";
@@ -17,23 +17,21 @@ import { Toaster } from "react-hot-toast";
 
 export default function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50 pb-20">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/arama" element={<Arama />} />
-          <Route path="/magaza" element={<Magaza />} />
-          <Route path="/ilan-ver" element={<NewListing />} />
-          <Route path="/onizleme" element={<Onizleme />} />
-          <Route path="/profilim" element={<Profilim />} />
-          <Route path="/bana-ozel" element={<BanaOzel />} />
-          <Route path="/index.html" element={<Navigate to="/" replace />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+    <div className="min-h-screen bg-gray-50 pb-20">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/arama" element={<Arama />} />
+        <Route path="/magaza" element={<Magaza />} />
+        <Route path="/ilan-ver" element={<NewListing />} />
+        <Route path="/onizleme" element={<Onizleme />} />
+        <Route path="/profilim" element={<Profilim />} />
+        <Route path="/bana-ozel" element={<BanaOzel />} />
+        <Route path="/index.html" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
 
-        <BottomTabs />
-      </div>
+      <BottomTabs />
       <Toaster position="bottom-center" />
-    </Router>
+    </div>
   );
 }
